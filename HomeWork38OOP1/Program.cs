2cc5aa24209d44e4b2ebd317d4c73b949160c4f5
An-124Rus <111499611+An-124Rus@users.noreply.github.com>
2023-01-17 18:45:32 +0300

    HomeWork38OOP1

---
+++ HomeWork38OOP1/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Player player = new Player("Иван", 18, 15);
+
+        player.ShowInfo();
+    }
+}
+
+class Player
+{
+    public string Name;
+    public int Age;
+    public int Level;
+
+    public Player(string name, int age, int level)
+    {
+        Name = name;
+        Age = age;
+        Level = level;
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Имя игрока: {Name}\nВозраст: {Age}\nУровень: {Level}");
+    }
+}
